b8dd45d4e8b982bea10d7bdb2891c0e6b08678a4
SsnAgo <sunshounan@cai-inc.com>
2024-05-08 14:18:19 +0800

    feat: 根据库表生成model对象

---
+++ gen/model/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"chaindraw-fair-ticket-backend/global"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+	"strings"
+)
+
+// 根据库表重新生成数据库model
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "model",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+	g.UseDB(global.DB)
+
+	var dataMap = map[string]func(gorm.ColumnType) (dataType string){
+		// int mapping
+		"int": func(columnType gorm.ColumnType) (dataType string) {
+			if n, ok := columnType.Nullable(); ok && n {
+				return "*int64"
+			}
+			return "int64"
+		},
+
+		// bool mapping
+		"tinyint": func(columnType gorm.ColumnType) (dataType string) {
+			ct, _ := columnType.ColumnType()
+			if strings.HasPrefix(ct, "tinyint(1)") {
+				return "*bool"
+			}
+			return "byte"
+		},
+	}
+	g.WithDataTypeMap(dataMap)
+
+	// 定义json tag命名格式
+	g.GenerateAllTable(gen.FieldJSONTagWithNS(func(columnName string) (tagContent string) {
+		arr := strings.Split(columnName, "_")
+		tagContent = arr[0]
+		for i := 1; i < len(arr); i++ {
+			word := upFirst(arr[i])
+			tagContent += word
+		}
+		return tagContent
+	}))
+
+	// Generate the code
+	g.Execute()
+
+}
+
+func upFirst(str string) (word string) {
+	byteArr := []byte(str)
+	byteArr[0] -= 32
+	word = string(byteArr)
+	return word
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/spf13/viper v1.18.2
 	go.uber.org/zap v1.27.0
 	gorm.io/driver/mysql v1.5.6
+	gorm.io/gen v0.3.26
 	gorm.io/gorm v1.25.10
 )
 
@@ -49,11 +50,16 @@ require (
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/datatypes v1.1.1-0.20230130040222-c43177d3cf8c // indirect
+	gorm.io/hints v1.1.0 // indirect
+	gorm.io/plugin/dbresolver v1.5.0 // indirect
 )
