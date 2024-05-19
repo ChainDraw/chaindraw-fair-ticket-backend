b1d2407109a7ca761a194db4dcd53cf3fe827c35
Austin Wang <1732747279@qq.com>
2024-05-20 05:27:18 +0800

    帮助asta合并最新进度

--- api/v1/chaindraw/concert.go
+++ api/v1/chaindraw/concert.go
@@ -1,18 +1,21 @@
 /*
- * @author: biturd
- * @date: 2024/5/12 09:00
- * @description:
+ * @author: asta
+ * @date: 2024/5/13 23:27
+ * @description: concert controller
+ *
  */
 package chaindraw
 
 import (
 	"chaindraw-fair-ticket-backend/global"
+	commonreq "chaindraw-fair-ticket-backend/model/common/request"
 	commonresp "chaindraw-fair-ticket-backend/model/common/response"
 	"chaindraw-fair-ticket-backend/service"
-	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 	"net/http"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func ConcertList(ctx *gin.Context) {
@@ -23,6 +26,20 @@ func ConcertList(ctx *gin.Context) {
 	resp.Result = concerts
 	if err != nil {
 		global.LOGGER.Error("query concerts logic failed", zap.Any("ids", ids), zap.Error(err))
+		ctx.JSON(http.StatusBadRequest, resp)
+	}
+
+	ctx.JSON(http.StatusOK, resp)
+}
+
+func ReviewConcert(ctx *gin.Context) {
+	req := &commonreq.ConcertReViewReq{}
+	resp := &commonresp.ConcertListResponse{}
+	concerts, err := service.ConcertReview(req)
+
+	resp.Result = concerts
+	if err != nil {
+		global.LOGGER.Error("review concerts logic failed", zap.Error(err))
 		ctx.JSON(http.StatusBadRequest, resp)
 	}
 
