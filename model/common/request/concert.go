a068793a8c6db6371528ed3310a286e136aa6406
Austin Wang <1732747279@qq.com>
2024-05-28 00:04:10 +0800

    add a requestion for concert cancel router.

--- model/common/request/concert.go
+++ model/common/request/concert.go
@@ -9,3 +9,8 @@ type ConcertReViewReq struct {
 	ConcertID string `json:"concert_id"` // 提交的演唱会id
 	Pass      bool   `json:"pass"`       // 是否通过
 }
+
+type ConcertCancelReq struct {
+	ConcertID    string `json:"concert_id"`    // 提交的演唱会id
+	CancelReason string `json:"cancel_reason"` // 是否通过
+}
